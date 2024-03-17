51ee2e81a563384f084b912e10e2ca3fad65d1cd
yujimiya <yuji.miyamoto32@gmail.com>
2024-03-17 21:35:28 +0900

    一覧機能の実装完了 ①コントローラーの変更 ・ルートパスが指定された場合にメソッドが実行される ・イシュー全件のデータを「issueList」という変数に格納する ・issueListの中身は、リポジトリの「findAll」というメソッドを用いて取得する 表示するHTMLの名称は「index」とする ②イシューを格納するためのエンティティの作成 作成するエンティティの仕様 クラス名 IssueEntity
    エンティティに含まれるフィールド
    
    データ型	変数名
    long	id
    String	title
    String	content
    String	period
    char	importance
    ③リポジトリの変更
    変更後のリポジトリの仕様
    追加するメソッド名
    findAll
    
    追加するメソッドの仕様
    - issuesテーブルからデータを全件取得して、結果をIssueEntity型のリストで返す。
    ④ビューの作成
    以下の内容でHTMLファイルを作成する
    
    作成場所：src/main/resources/templates
    ファイル名：index.html

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -1,7 +1,9 @@
 package in.techcamp.issueapp;
 
 import lombok.AllArgsConstructor;
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -21,5 +23,12 @@ public class IssueController {
     public String createIssue(IssueForm issueForm){
         issueRepository.insert(issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
         return "redirect:/";
+    }
+
+    @GetMapping
+    public String showIssues(Model model){
+        var issueList = issueRepository.findAll();
+        model.addAttribute("issueList", issueList);
+        return "index";
     }
 }
