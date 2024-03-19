7b9f612797d3dc02e664dcb04289c2d8d4e45fb1
yujimiya <yuji.miyamoto32@gmail.com>
2024-03-19 22:03:30 +0900

    更新機能の実装 ①今ローラーの編集 データ更新用のメソッドを追加 @PostMapping("/issues/{id}/update")     public String updateIssue(@PathVariable long id, IssueForm issueForm){         issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());         return "redirect:/";     } ②リポジトリを編集 データ更新用のメソッドを作成するため、以下のようにリポジトリを変更  @Update("UPDATE issues SET title = #{title}, content = #{content}, period = #{period}, importance = #{importance} WHERE id =#{id}")     void update(long id, String title, String content, String period, char importance); ③ビューの編集 一覧画面から詳細画面に遷移できるよう、以下のように変更  <td><a th:text="${issue.getTitle()}" th:href="@{/issues/{id}(id=${issue.getId()})}"></a></td>

--- src/main/java/in/techcamp/issueapp/IssueController.java
+++ src/main/java/in/techcamp/issueapp/IssueController.java
@@ -39,4 +39,10 @@ public class IssueController {
         model.addAttribute("issue", issue);
         return "detail";
     }
+
+    @PostMapping("/issues/{id}/update")
+    public String updateIssue(@PathVariable long id, IssueForm issueForm){
+        issueRepository.update(id, issueForm.getTitle(), issueForm.getContent(), issueForm.getPeriod(), issueForm.getImportance());
+        return "redirect:/";
+    }
 }
